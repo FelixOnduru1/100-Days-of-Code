a36096e3c997aba132d2099eb8bfc682a6402a19
FelixOnduru1 <72564957+FelixOnduru1@users.noreply.github.com>
2021-01-20 15:47:56 +0300

    printing and concatenation

--- Day1/main.py
+++ Day1/main.py
@@ -1,1 +1,5 @@
-print("Day 1 - Python Print Function\nThe function is declaed like this:\nprint('what to print')")
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+print("Day 1 - String Manipulation")
+print('String concatenation is done using the "+" sign')
+print('E.g print ("Hello" + "World!")')
+print("New lines can be created wih a backlash and n")
