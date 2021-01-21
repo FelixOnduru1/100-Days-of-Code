f360db2b899b02e19ac258772ed62e1392c56768
FelixOnduru1 <72564957+FelixOnduru1@users.noreply.github.com>
2021-01-21 14:07:55 +0300

    created life-in-weeks.py program.

--- Day2/life-in-weeks.py
+++ Day2/life-in-weeks.py
@@ -8,4 +8,4 @@ weeks_left = years_left*52
 days_left = years_left*365
 
 print(f"You only have {months_left} months left.\nYou only have {weeks_left} weeks left."
-      f"\nYou only have {days_left} days left.")
+      f"\nYou only have {days_left} days left.af")
