18e3b99794dc7530c97aaf35dddc5120bb777018
FelixOnduru1 <72564957+FelixOnduru1@users.noreply.github.com>
2021-01-22 08:40:08 +0300

    Updated rollercoaster.py to include age factor. Worked perferct.

--- Day3/rollercoaster.py
+++ Day3/rollercoaster.py
@@ -1,7 +1,14 @@
 print("Welcome to the Rollercoaster.\n"
-      "This program checks for height and determines whether you can ride or not.")
+      "This program checks for height and determines whether you can ride or not and the amount to pay.")
 height = int(input("What is your height in cm?\n"))
+age = int(input("What is your age?\n"))
 if height >= 120:
-    print("You can ride the Rollercoaster. Enjoy!!")
+    print("You can ride the Rollercoaster.")
+    if age < 12:
+        print("You will pay $5.00 to ride. Enjoy!!")
+    elif 12 <= age <= 18:
+        print("You will pay $7:00 to ride. Enjoy!!")
+    else:
+        print("You will pay $12.00 to ride. Enjoy!!")
 else:
-    print("Sorry, you can't ride!")
+    print(f"Sorry, you can't ride! You need to grow {120-height}cm to ride.")
