23f8d9c2e6f1b899245854eee2dc161aaec6600b
FelixOnduru1 <72564957+FelixOnduru1@users.noreply.github.com>
2021-01-22 07:40:27 +0300

    Created rollercoaster.py and the program runs perfectly fine.

---
+++ Day3/rollercoaster.py
@@ -0,0 +1,7 @@
+print("Welcome to the Rollercoaster.\n"
+      "This program checks for height and determines whether you can ride or not.")
+height = int(input("What is your height in cm?\n"))
+if height >= 120:
+    print("You can ride the Rollercoaster. Enjoy!!")
+else:
+    print("Sorry, you can't ride!")
