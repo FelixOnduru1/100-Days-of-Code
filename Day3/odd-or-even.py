0ff2cf218054929afa8bd098dd869c2ea875f7b2
FelixOnduru1 <72564957+FelixOnduru1@users.noreply.github.com>
2021-01-22 07:47:56 +0300

    Created odd-or-even.py and the project worked fine.

---
+++ Day3/odd-or-even.py
@@ -0,0 +1,6 @@
+print("Welcome.\nThis program tells you whether a number is odd or even.")
+number = int(input("Enter your number?\n"))
+if number % 2 == 0:
+    print("The number you entered is even.")
+else:
+    print("The number you entered is odd.")
