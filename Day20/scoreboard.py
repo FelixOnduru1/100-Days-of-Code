337acfb65d78fe882019dfa33d3b4c5bf95d7dc0
FelixOnduru1 <72564957+FelixOnduru1@users.noreply.github.com>
2021-02-24 04:27:54 +0300

    Snake game improved to store high score

--- Day20/scoreboard.py
+++ Day20/scoreboard.py
@@ -2,13 +2,16 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 15, "normal")
 
+with open("data.txt") as data:
+    HIGH_SCORE = int(data.read())
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = HIGH_SCORE
         self.penup()
         self.hideturtle()
         self.color("white")
@@ -27,5 +30,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
