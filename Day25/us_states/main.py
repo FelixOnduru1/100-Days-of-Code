7cad573c18f68b969de598c5041d0d4b4fe0dfb8
FelixOnduru1 <72564957+FelixOnduru1@users.noreply.github.com>
2021-02-25 20:41:46 +0300

    Created the US States Game

---
+++ Day25/us_states/main.py
@@ -0,0 +1,36 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+img = "blank_states_image.gif"
+screen.addshape(img)
+turtle.shape(img)
+data = pd.read_csv("50_states.csv")
+states = data.state.to_list()
+guessed_state = []
+game_active = True
+
+while game_active:
+    answer_state = screen.textinput(title=f"Guess a State. {len(guessed_state)}/50", prompt="Type a state name").title()
+
+    if len(guessed_state) > 50 or answer_state == "Exit":
+        missed_state = []
+        for state in states:
+            if state not in guessed_state:
+                missed_state.append(state)
+            missed_state_data = pd.DataFrame(missed_state)
+            missed_state_data.to_csv("missed_states.csv")
+        game_active = False
+        break
+
+    if answer_state in states and answer_state not in guessed_state:
+        guessed_state.append(answer_state)
+        state_data = data[data.state == answer_state]
+        xcor = int(state_data.x)
+        ycor = int(state_data.y)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(xcor, ycor)
+        t.write(answer_state)
