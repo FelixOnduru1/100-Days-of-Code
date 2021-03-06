92cb82a6c825f83f4c85f109a5de763927b4e6f5
FelixOnduru1 <72564957+FelixOnduru1@users.noreply.github.com>
2021-03-06 15:31:36 +0300

    Updated NATO to print error message in case words entered not in alphabet.

--- Day26/NATO/main.py
+++ Day26/NATO/main.py
@@ -3,8 +3,17 @@ df = pd.read_csv("nato_phonetic_alphabet.csv")
 
 alphabet_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-user_word = input("Enter a word:\n").upper()
 
-user_word_list = [alphabet_dict[letter] for letter in user_word]
+def generate_phonetic():
+    user_word = input("Enter a word:\n").upper()
 
-print(user_word_list)
+    try:
+        user_word_list = [alphabet_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry! Only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(user_word_list)
+
+
+generate_phonetic()
